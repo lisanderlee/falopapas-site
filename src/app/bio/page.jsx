1ac54e315cd40c6ce36f8faff705e875669cfe21
lisanderlee <lisander.lee@gmail.com>
2023-08-04 11:39:53 -0400

    coming soon

--- src/app/bio/page.jsx
+++ src/app/bio/page.jsx
@@ -1,26 +1,31 @@
-
-
+import Link from "next/link";
+import Navbar from "../Component/Navbar";
 export default function Bio() {
   return (
-    <div className="relative h-full justify-between mt-6 items-center  px-5 lg:px-20">
-      <header class="df aic jcc">
-        <div>
-          <h1 className=" text-white font-bold">
-            Welcome to Falopapas Studio,
-          </h1>
-          <h2 className="text-white font-bold">
-            a Miami-based haven of contemporary art. Run by the accomplished
-            Argentine artist and visionary, Mr. Falopapas, our studio offers a
-            unique blend of urban sophistication and modern subjects. With a
-            degree in Fine Arts and a thriving career in painting and public art
-            since 2005, Néstor Augusto Turallas, aka Falopapas, invites you to
-            explore his captivating world of creativity and inspiration.
-            <button className=" ml-1 font-bold hover:bg-white text-purple-600 text-md hover:text-black outline-white px-5 py-2 rounded-full ">
-              Learn More
-            </button>
-          </h2>
-        </div>
-      </header>
+    <>
+    <Navbar />
+    <div className=" mt-28 mb-36 h-full justify-between flex items-center px-4        lg:mt-64 lg:px-20">
+      <div>
+        <h1 className=" text-6xl h-full leading-tight text-white font-bold mb-5 lg:text-8xl">
+        Coming Soon
+        </h1>
+        {/* <h2 className="text-white text-lg font-base leading-loose md:text-2xl lg:text-4xl lg:leading-relaxed">
+        Welcome to Falopapas Studio, a Miami-based haven of contemporary art. Run by the accomplished
+          Argentine artist and visionary, Mr. Falopapas, our studio offers a
+          unique blend of urban sophistication and modern subjects. With a
+          degree in Fine Arts and a thriving career in painting and public art
+          since 2005, Néstor Augusto Turallas, aka Falopapas, invites you to
+          explore his captivating world of creativity and inspiration.
+        </h2>
+        <div className="outline text-white hover:bg-white hover:text-black outline-white px-5 py-2 w-36 mt-5 rounded-full">
+        <Link
+          href="/bio/"
+        >
+          Learn More
+        </Link>
+        </div> */}
+      </div>
     </div>
+    </>
   );
 }
